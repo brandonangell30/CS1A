a7952e7f6dbab2a4ee25daa29b47edfcee294790
Brandon <157135813+brandonangell30@users.noreply.github.com>
2025-02-25 11:06:58 -0500

    Add files via upload

---
+++ Lab1/greeting.cpp
@@ -0,0 +1,11 @@
+//displays a greeting
+//Brandon Angell
+//08-22-2023
+
+#include <iostream>
+
+using std::cout; using std::endl;
+
+int main() {
+	cout << "Hello, Brandon Angell! How are you?" << endl;
+}
